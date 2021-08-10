de3c46b774052a60977a6fc88071cdb4bb946c9d
Jhonatas-P-Santos <jhonata.1521@hotmail.com>
2021-08-10 16:11:25 -0300

    frist commit

---
+++ src/br/bom/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.bom.dio;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+         int a = 5;
+         int b = 3;
+        System.out.println("Soma= " + (a+b));
+
+    }
+
+
+}
